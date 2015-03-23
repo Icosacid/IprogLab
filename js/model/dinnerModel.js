f1b1a9e86522c3ae1c339e45f451516322f98bcc
Charly Fontaine <charlyf137@gmail.com>
2015-03-23 08:58:09 +0100

    test

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -14,7 +14,7 @@ var DinnerModel = function() {
  	};
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
-	this.guests = 1;
+	this.guests = 2;
 	this.selected = {
 		SaladID : 0,
 		mainDishID : 0,
